615e15f8db6e132962df275453b386310b2f633e
Yash Izate <yashizate.softech@gmail.com>
2026-07-13 04:25:02 +0530

    java patterns

--- src/pattern/InvertedRightAngledTriangle.java
+++ src/pattern/InvertedRightAngledTriangle.java
@@ -1,12 +1,11 @@
 package pattern;
 
-public class SolidRightAngledTriangle {
+public class InvertedRightAngledTriangle {
     public static void main(String[] args) {
-
         int n = 5;
 
-        for (int i = 1; i <= 5 ; i++) {
-            for (int j = 1; j <= i ; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i +1 ; j++) {
                 System.out.print("* ");
             }
             System.out.println();
--- src/pattern/SolidSquare.java
+++ src/pattern/SolidSquare.java
@@ -1,14 +1,5 @@
 package pattern;
 
-/*
-
-* * * *
-* * * *
-* * * *
-* * * *
-
- */
-
 public class SolidSquare {
     public static void main(String[] args) {
 
