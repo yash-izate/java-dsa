d2e05f62f49ef8f4e02df6e844e8f312cbe776bf
Yash Izate <yashizate.softech@gmail.com>
2026-07-12 16:53:49 +0530

    java dsa begin

--- src/basic/IsPrime.java
+++ src/basic/IsPrime.java
@@ -1,4 +1,10 @@
 package basic;
 
+import java.util.Scanner;
+
 public class IsPrime {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Welcome to Prime Number Checker!");
+    }
 }
