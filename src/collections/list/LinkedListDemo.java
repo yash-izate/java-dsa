3b76a2948758a5729265c63f1b8ee5555c7be910
Yash Izate <yashizate.softech@gmail.com>
2026-07-19 17:19:14 +0530

    java collections

--- src/collections/list/LinkedListDemo.java
+++ src/collections/list/LinkedListDemo.java
@@ -1,54 +1,51 @@
 package collections.list;
 
 import java.util.*;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class ArrayListDemo {
+public class LinkedListDemo {
     public static void main(String[] args) {
         // object can be made using reference of
-        Collection<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        ArrayList<Integer> l3 = new ArrayList<>();
-
-        // ensure the capacity
-        l3.ensureCapacity(100);
+        Collection<Integer> l1 = new LinkedList<>();
+        List<Integer> l2 = new LinkedList<>();
+        LinkedList<Integer> l3 = new LinkedList<>();
 
         // add elements to first list
-        l3.add(10);
-        l3.add(30);
         l3.add(50);
-        l3.add(70);
-        l3.add(90);
-        System.out.println("List 1:" + l3);
+        l3.add(25);
+        l3.add(75);
+        l3.add(105);
+        l3.add(15);
+        System.out.println("L3:" + l3);
 
         //remove element
-        l3.remove(2);
-        l3.removeFirst();
+        l3.remove(3);
         l3.removeLast();
-        System.out.println("List 1:" + l3);
+        l3.removeFirst();
+        System.out.println("L3:" + l3);
 
         // add elements using loop n second list
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             l2.add(i * 3);
             l2.add(i * 5);
         }
-        System.out.println("List 2: " + l2);
+        System.out.println("L2: " + l2);
 
         // addAll of l3 and l2 to l1
         l1.addAll(l3);
         l1.addAll(l2);
-        System.out.println("List 3: " + l1);
+        System.out.println("L1: " + l1);
 
         // removeAll of all
         l1.removeAll(l3);
-        System.out.println("List 3: " + l1);
+        System.out.println("L3: " + l1);
 
         // list size
         System.out.println("Size of L1: " + l1.size());
 
         // clear list
         l3.clear();
-        System.out.print("List 3: ");
+        System.out.print("List 3: " + l3);
 
         // get method
         System.out.println(l2.get(1));
@@ -64,33 +61,32 @@ public class ArrayListDemo {
         // convert to array using toArray
         Object[] arr = l1.toArray();
         for (Object ele : arr) {
-            System.out.println("Array: " + ele);
+            System.out.println("LL: " + ele);
 
         }
 
         // is contained -> true or false
         System.out.println(l1);
-        System.out.println(l1.contains(9));
+        System.out.println(l1.contains(6));
 
         // sorting
         System.out.println(l2);
         Collections.sort(l2);
-        System.out.println("Sort List: " + l2);
+        System.out.println("Sort List : " + l2);
 
         // isEmpty
         System.out.println(l2.isEmpty());
         System.out.println(l2);
-        System.out.println(l2.indexOf(12));
+        System.out.println(l2.indexOf(6));
 
         // clone list - (Note - it will only work if reference type is Arraylist only, not list or collection  )
-        l3.add(900);
-        l3.add(200);
-        l3.add(500);
-        ArrayList<Integer> newList = (ArrayList<Integer>) l3.clone();
+        l3.add(700);
+        l3.add(300);
+        l3.add(100);
+        LinkedList<Integer> newList = (LinkedList<Integer>) l3.clone();
         System.out.println("New Cloned List: " + newList);
 
-        // starting min capacity of list
-        newList.ensureCapacity(500);
+        // sort
         Collections.sort(newList);
         System.out.println(newList);
 
