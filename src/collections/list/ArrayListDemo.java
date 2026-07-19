58dde277b49dd00b05332ae26ec1549648eb7b99
Yash Izate <yashizate.softech@gmail.com>
2026-07-19 17:37:29 +0530

    java collections

--- src/collections/list/ArrayListDemo.java
+++ src/collections/list/ArrayListDemo.java
@@ -1,98 +1,169 @@
 package collections.list;
 
 import java.util.*;
-import java.util.ArrayList;
 
 public class ArrayListDemo {
+
     public static void main(String[] args) {
-        // object can be made using reference of
+
+        // Object can be created using different reference types
         Collection<Integer> l1 = new ArrayList<>();
         List<Integer> l2 = new ArrayList<>();
         ArrayList<Integer> l3 = new ArrayList<>();
 
-        // ensure the capacity
+        // ---------------- CAPACITY ----------------
         l3.ensureCapacity(100);
 
-        // add elements to first list
+        // ---------------- ADD METHODS ----------------
         l3.add(10);
         l3.add(30);
         l3.add(50);
         l3.add(70);
         l3.add(90);
-        System.out.println("List 1:" + l3);
 
-        //remove element
-        l3.remove(2);
-        l3.removeFirst();
-        l3.removeLast();
-        System.out.println("List 1:" + l3);
+        l3.add(2, 40);                 // Add at index
 
-        // add elements using loop n second list
+        System.out.println("After Adding Elements : " + l3);
+
+        // ---------------- ACCESS METHODS ----------------
+        System.out.println("\n----- Access Methods -----");
+
+        System.out.println("get(2)        : " + l3.get(2));
+        System.out.println("First Element : " + l3.getFirst());   // Java 21+
+        System.out.println("Last Element  : " + l3.getLast());    // Java 21+
+
+        // ---------------- SEARCH METHODS ----------------
+        System.out.println("\n----- Search Methods -----");
+
+        System.out.println("contains(50)  : " + l3.contains(50));
+        System.out.println("indexOf(70)   : " + l3.indexOf(70));
+        System.out.println("lastIndexOf(70): " + l3.lastIndexOf(70));
+
+        // ---------------- UPDATE ----------------
+        l3.set(2, 45);
+
+        System.out.println("\nAfter set()   : " + l3);
+
+        // ---------------- REMOVE METHODS ----------------
+        l3.remove(2);                  // Remove by index
+        l3.remove(Integer.valueOf(70));// Remove by object
+        l3.removeFirst();              // Java 21+
+        l3.removeLast();               // Java 21+
+
+        System.out.println("\nAfter Removing Elements : " + l3);
+
+        // ---------------- SECOND LIST ----------------
         for (int i = 0; i < 5; i++) {
             l2.add(i * 3);
             l2.add(i * 5);
         }
-        System.out.println("List 2: " + l2);
 
-        // addAll of l3 and l2 to l1
+        System.out.println("\nSecond List : " + l2);
+
+        // ---------------- BULK OPERATIONS ----------------
         l1.addAll(l3);
         l1.addAll(l2);
-        System.out.println("List 3: " + l1);
 
-        // removeAll of all
+        System.out.println("\nAfter addAll() : " + l1);
+
         l1.removeAll(l3);
-        System.out.println("List 3: " + l1);
 
-        // list size
-        System.out.println("Size of L1: " + l1.size());
+        System.out.println("After removeAll() : " + l1);
 
-        // clear list
-        l3.clear();
-        System.out.print("List 3: ");
+        ArrayList<Integer> common = new ArrayList<>();
+        common.add(0);
+        common.add(3);
 
-        // get method
-        System.out.println(l2.get(1));
+        System.out.println("containsAll() : " + l2.containsAll(common));
 
+        l2.retainAll(common);
 
-        // iterator
+        System.out.println("After retainAll() : " + l2);
+
+        // ---------------- SIZE ----------------
+        System.out.println("\nSize : " + l1.size());
+
+        // ---------------- ITERATOR ----------------
+        System.out.println("\n----- Iterator -----");
+
         Iterator<Integer> iterator = l1.iterator();
 
         while (iterator.hasNext()) {
-            System.out.print("L1 elements: " + iterator.next());
+            System.out.print(iterator.next() + " ");
         }
 
-        // convert to array using toArray
+        // ---------------- ENHANCED FOR LOOP ----------------
+        System.out.println("\n\n----- Enhanced For Loop -----");
+
+        for (Integer num : l1) {
+            System.out.print(num + " ");
+        }
+
+        // ---------------- FOREACH ----------------
+        System.out.println("\n\n----- forEach() -----");
+
+        l1.forEach(num -> System.out.print(num + " "));
+
+        // ---------------- TO ARRAY ----------------
+        System.out.println("\n\n----- toArray() -----");
+
         Object[] arr = l1.toArray();
-        for (Object ele : arr) {
-            System.out.println("Array: " + ele);
 
+        for (Object obj : arr) {
+            System.out.println(obj);
         }
 
-        // is contained -> true or false
-        System.out.println(l1);
-        System.out.println(l1.contains(9));
+        // ---------------- SUBLIST ----------------
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(100);
+        temp.add(200);
+        temp.add(300);
+        temp.add(400);
+        temp.add(500);
 
-        // sorting
-        System.out.println(l2);
-        Collections.sort(l2);
-        System.out.println("Sort List: " + l2);
+        System.out.println("\nOriginal List : " + temp);
+        System.out.println("SubList : " + temp.subList(1, 4));
 
-        // isEmpty
-        System.out.println(l2.isEmpty());
-        System.out.println(l2);
-        System.out.println(l2.indexOf(12));
+        // ---------------- REMOVE IF ----------------
+        temp.removeIf(n -> n > 300);
 
-        // clone list - (Note - it will only work if reference type is Arraylist only, not list or collection  )
-        l3.add(900);
-        l3.add(200);
-        l3.add(500);
-        ArrayList<Integer> newList = (ArrayList<Integer>) l3.clone();
-        System.out.println("New Cloned List: " + newList);
+        System.out.println("After removeIf() : " + temp);
 
-        // starting min capacity of list
-        newList.ensureCapacity(500);
-        Collections.sort(newList);
-        System.out.println(newList);
+        // ---------------- REPLACE ALL ----------------
+        temp.replaceAll(n -> n * 10);
 
+        System.out.println("After replaceAll() : " + temp);
+
+        // ---------------- CLONE ----------------
+        ArrayList<Integer> clonedList =
+                (ArrayList<Integer>) temp.clone();
+
+        System.out.println("\nCloned List : " + clonedList);
+
+        // ---------------- COLLECTIONS METHODS ----------------
+        Collections.sort(clonedList);
+        System.out.println("\nSorted      : " + clonedList);
+
+        Collections.reverse(clonedList);
+        System.out.println("Reverse     : " + clonedList);
+
+        Collections.shuffle(clonedList);
+        System.out.println("Shuffle     : " + clonedList);
+
+        System.out.println("Maximum     : " + Collections.max(clonedList));
+        System.out.println("Minimum     : " + Collections.min(clonedList));
+
+        Collections.fill(clonedList, 999);
+        System.out.println("Fill        : " + clonedList);
+
+        // ---------------- CAPACITY METHODS ----------------
+        clonedList.ensureCapacity(500);
+        clonedList.trimToSize();
+
+        // ---------------- CLEAR ----------------
+        clonedList.clear();
+
+        System.out.println("\nAfter clear() : " + clonedList);
+        System.out.println("isEmpty()     : " + clonedList.isEmpty());
     }
 }
--- src/collections/list/LinkedListDemo.java
+++ src/collections/list/LinkedListDemo.java
@@ -1,94 +1,202 @@
 package collections.list;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class LinkedListDemo {
+
     public static void main(String[] args) {
-        // object can be made using reference of
-        Collection<Integer> l1 = new LinkedList<>();
-        List<Integer> l2 = new LinkedList<>();
-        LinkedList<Integer> l3 = new LinkedList<>();
 
-        // add elements to first list
-        l3.add(50);
-        l3.add(25);
-        l3.add(75);
-        l3.add(105);
-        l3.add(15);
-        System.out.println("L3:" + l3);
+        // Creating LinkedList
+        LinkedList<Integer> list = new LinkedList<>();
 
-        //remove element
-        l3.remove(3);
-        l3.removeLast();
-        l3.removeFirst();
-        System.out.println("L3:" + l3);
+        // ---------------- ADD METHODS ----------------
+        list.add(20);
+        list.add(40);
+        list.add(60);
 
-        // add elements using loop n second list
-        for (int i = 0; i < 6; i++) {
-            l2.add(i * 3);
-            l2.add(i * 5);
+        list.addFirst(10);
+        list.addLast(80);
+
+        list.offer(100);
+        list.offerFirst(5);
+        list.offerLast(120);
+
+        list.add(3, 30);
+
+        System.out.println("After Adding Elements : " + list);
+
+        // ---------------- ACCESS METHODS ----------------
+        System.out.println("\n----- Access Methods -----");
+        System.out.println("get(2)        : " + list.get(2));
+        System.out.println("getFirst()    : " + list.getFirst());
+        System.out.println("getLast()     : " + list.getLast());
+        System.out.println("peek()        : " + list.peek());
+        System.out.println("peekFirst()   : " + list.peekFirst());
+        System.out.println("peekLast()    : " + list.peekLast());
+        System.out.println("element()     : " + list.element());
+
+        // ---------------- SEARCH METHODS ----------------
+        System.out.println("\n----- Search Methods -----");
+        System.out.println("contains(60)  : " + list.contains(60));
+        System.out.println("indexOf(60)   : " + list.indexOf(60));
+        System.out.println("lastIndexOf(60): " + list.lastIndexOf(60));
+
+        // ---------------- UPDATE ----------------
+        list.set(2, 25);
+        System.out.println("\nAfter set()   : " + list);
+
+        // ---------------- REMOVE METHODS ----------------
+        System.out.println("\n----- Remove Methods -----");
+
+        list.removeFirst();
+        System.out.println("removeFirst() : " + list);
+
+        list.removeLast();
+        System.out.println("removeLast()  : " + list);
+
+        list.remove(Integer.valueOf(40));
+        System.out.println("remove(40)    : " + list);
+
+        list.remove(2);
+        System.out.println("remove(index) : " + list);
+
+        // ---------------- STACK METHODS ----------------
+        System.out.println("\n----- Stack Methods -----");
+
+        list.push(99);
+        System.out.println("push(99)      : " + list);
+
+        System.out.println("pop()         : " + list.pop());
+        System.out.println("After pop     : " + list);
+
+        // ---------------- QUEUE METHODS ----------------
+        System.out.println("\n----- Queue Methods -----");
+
+        System.out.println("offer(500)");
+        list.offer(500);
+
+        System.out.println("peek()        : " + list.peek());
+
+        System.out.println("poll()        : " + list.poll());
+
+        System.out.println("After poll    : " + list);
+
+        // ---------------- SIZE ----------------
+        System.out.println("\nSize          : " + list.size());
+
+        // ---------------- ITERATOR ----------------
+        System.out.println("\n----- Iterator -----");
+
+        Iterator<Integer> itr = list.iterator();
+
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
         }
-        System.out.println("L2: " + l2);
 
-        // addAll of l3 and l2 to l1
-        l1.addAll(l3);
-        l1.addAll(l2);
-        System.out.println("L1: " + l1);
+        // ---------------- LIST ITERATOR ----------------
+        System.out.println("\n\n----- ListIterator (Forward) -----");
 
-        // removeAll of all
-        l1.removeAll(l3);
-        System.out.println("L3: " + l1);
+        ListIterator<Integer> li = list.listIterator();
 
-        // list size
-        System.out.println("Size of L1: " + l1.size());
-
-        // clear list
-        l3.clear();
-        System.out.print("List 3: " + l3);
-
-        // get method
-        System.out.println(l2.get(1));
-
-
-        // iterator
-        Iterator<Integer> iterator = l1.iterator();
-
-        while (iterator.hasNext()) {
-            System.out.print("L1 elements: " + iterator.next());
+        while (li.hasNext()) {
+            System.out.print(li.next() + " ");
         }
 
-        // convert to array using toArray
-        Object[] arr = l1.toArray();
-        for (Object ele : arr) {
-            System.out.println("LL: " + ele);
+        System.out.println("\n\n----- ListIterator (Backward) -----");
 
+        while (li.hasPrevious()) {
+            System.out.print(li.previous() + " ");
         }
 
-        // is contained -> true or false
-        System.out.println(l1);
-        System.out.println(l1.contains(6));
+        // ---------------- DESCENDING ITERATOR ----------------
+        System.out.println("\n\n----- Descending Iterator -----");
 
-        // sorting
-        System.out.println(l2);
-        Collections.sort(l2);
-        System.out.println("Sort List : " + l2);
+        Iterator<Integer> desc = list.descendingIterator();
 
-        // isEmpty
-        System.out.println(l2.isEmpty());
-        System.out.println(l2);
-        System.out.println(l2.indexOf(6));
+        while (desc.hasNext()) {
+            System.out.print(desc.next() + " ");
+        }
 
-        // clone list - (Note - it will only work if reference type is Arraylist only, not list or collection  )
-        l3.add(700);
-        l3.add(300);
-        l3.add(100);
-        LinkedList<Integer> newList = (LinkedList<Integer>) l3.clone();
-        System.out.println("New Cloned List: " + newList);
+        // ---------------- CLONE ----------------
+        LinkedList<Integer> cloned =
+                (LinkedList<Integer>) list.clone();
 
-        // sort
-        Collections.sort(newList);
-        System.out.println(newList);
+        System.out.println("\n\nCloned List   : " + cloned);
 
+        // ---------------- ADD ALL ----------------
+        LinkedList<Integer> list2 = new LinkedList<>();
+
+        list2.add(1000);
+        list2.add(2000);
+        list2.add(3000);
+
+        list.addAll(list2);
+
+        System.out.println("\nAfter addAll  : " + list);
+
+        // ---------------- REMOVE ALL ----------------
+        list.removeAll(list2);
+
+        System.out.println("After removeAll : " + list);
+
+        // ---------------- RETAIN ALL ----------------
+        LinkedList<Integer> list3 = new LinkedList<>();
+
+        list3.add(20);
+        list3.add(80);
+
+        list.retainAll(list3);
+
+        System.out.println("After retainAll : " + list);
+
+        // ---------------- CONTAINS ALL ----------------
+        System.out.println("containsAll() : " + list.containsAll(list3));
+
+        // ---------------- TO ARRAY ----------------
+        Object[] arr = list.toArray();
+
+        System.out.println("\nArray Elements");
+
+        for (Object i : arr) {
+            System.out.println(i);
+        }
+
+        // ---------------- SORT ----------------
+        list.clear();
+
+        list.add(50);
+        list.add(10);
+        list.add(90);
+        list.add(30);
+        list.add(70);
+
+        Collections.sort(list);
+
+        System.out.println("\nSorted List   : " + list);
+
+        // ---------------- REVERSE ----------------
+        Collections.reverse(list);
+
+        System.out.println("Reverse List  : " + list);
+
+        // ---------------- SHUFFLE ----------------
+        Collections.shuffle(list);
+
+        System.out.println("Shuffle List  : " + list);
+
+        // ---------------- MAX & MIN ----------------
+        System.out.println("Maximum       : " + Collections.max(list));
+        System.out.println("Minimum       : " + Collections.min(list));
+
+        // ---------------- CLEAR ----------------
+        list.clear();
+
+        System.out.println("\nAfter clear() : " + list);
+
+        // ---------------- EMPTY ----------------
+        System.out.println("isEmpty()     : " + list.isEmpty());
     }
 }
