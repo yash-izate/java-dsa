95cbd2f4636d513bbdffd1401c795344cc379917
Yash Izate <yashizate.softech@gmail.com>
2026-07-29 01:54:32 +0530

    bitwise  basics

---
+++ src/numbers/BitwiseEvenOdd.java
@@ -0,0 +1,14 @@
+package numbers;
+
+public class BitwiseEvenOdd {
+    public static void main(String[] args) {
+        int n = 12;
+
+        if ((n & 1) == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
+}
---
+++ src/numbers/SetBitCount.java
@@ -0,0 +1,29 @@
+package numbers;
+
+public class SetBitCount {
+    public static void main(String[] args) {
+        int n = 13;
+        int temp = n;
+        int count = 0;
+
+        // method easy
+        while (n != 0) {
+            count += (n & 1);
+            n = n >> 1;
+        }
+        System.out.println("Common: " + count);
+
+        // method function
+        System.out.println("Function: " + Integer.bitCount(temp));
+
+        // Brian Kernighan's Algorithm
+        int num = 13;
+        int i = 0;
+
+        while (num != 0) {
+            num = num & (num - 1);
+            i++;
+        }
+        System.out.println("Algorithm = " + i);
+    }
+}
