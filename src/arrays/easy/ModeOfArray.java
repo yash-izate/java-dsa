9b6b893477f0e990ddb4dd58f30f7685a0bb19a7
Yash Izate <yashizate.softech@gmail.com>
2026-08-08 02:13:51 +0530

    array dsa

--- src/arrays/easy/ModeOfArray.java
+++ src/arrays/easy/ModeOfArray.java
@@ -1,4 +1,27 @@
 package arrays.easy;
 
 public class ModeOfArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 3, 2, 3, 5, 2, 9, 5, 2, 2};
+        int mode = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if (count > maxCount) {
+                mode = arr[i];
+                maxCount = count;
+            }
+        }
+
+        System.out.println("Mode of array = " + mode);
+    }
 }
--- src/arrays/easy/PrintExtremeElements.java
+++ src/arrays/easy/PrintExtremeElements.java
@@ -1,4 +1,30 @@
 package arrays.easy;
 
-public class PrintExremeELements {
+public class PrintExtremeElements {
+
+    static void printExtremeAlternate(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        System.out.print("New = ");
+        while (i <= j) {
+
+            if (i == j) {
+                System.out.print(arr[i]);
+                return;
+
+            } else {
+                System.out.print(arr[i++] + ", ");
+                System.out.print(arr[j--] + ", ");
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        printExtremeAlternate(arr);
+
+    }
+
 }
